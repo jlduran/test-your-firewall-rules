cbc91b6aaf32ae62248d35dbd354f21855d5bf25
Jose Luis Duran <jlduran@gmail.com>
2021-09-22 17:13:32 -0300

    Move tests to a separate directory

--- .setup/setup.sh
+++ .setup/setup.sh
@@ -14,7 +14,7 @@ jail -c name=wan persist vnet vnet.interface=${out}b
 jail -c name=lan persist vnet vnet.interface=${in}b
 
 # setup wan
-jexec wan ifconfig ${out}b ${wan_ifconfig_if0}
+jexec wan ifconfig ${out}b ${wan_ifconfig}
 
 # setup firewall
 jexec firewall ifconfig ${out}a inet 192.0.2.2/30
@@ -27,5 +27,5 @@ jexec firewall pfctl -e
 jexec firewall pfctl -F all -f ${CIRRUS_WORKING_DIR}/pf.conf
 
 # setup lan
-jexec lan ifconfig ${in}b ${lan_ifconfig_if0}
+jexec lan ifconfig ${in}b ${lan_ifconfig}
 jexec lan route add default ${lan_defaultrouter}
---
+++ tests/test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+
+jexec lan ping -c 3 192.0.2.1
